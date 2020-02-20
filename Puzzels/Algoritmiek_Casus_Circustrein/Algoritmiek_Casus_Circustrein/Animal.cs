4a529b2ff486e56dfd64caf84a4c97782aad0182
Alex Peek <alex.peek@hotmail.com>
2020-02-20 08:57:41 +0100

    Added files from laptop

---
+++ Puzzels/Algoritmiek_Casus_Circustrein/Algoritmiek_Casus_Circustrein/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmiek_Casus_Circustrein
+{
+    class Animal
+    {
+        public static List<Animal> animalList = new List<Animal>();
+        private bool _carnivore;
+        private int _size;
+        public Animal(bool carnivore, int size)
+        {
+            this._carnivore = carnivore;
+            this._size = size;
+        }
+        public bool carnivore
+        {
+            get
+            {
+                return _carnivore;
+            }
+            set
+            {
+                _carnivore = value;
+            }
+        }
+        public int size
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                _size = value;
+            }
+        }
+    }
+}
---
+++ Puzzels/Algoritmiek_Casus_Circustrein/Algoritmiek_Casus_Circustrein/TrainWagon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmiek_Casus_Circustrein
+{
+    class TrainWagon
+    {
+        public static List<TrainWagon> trainWagonList = new List<TrainWagon>();
+        private bool _hasCarnivore;
+        private int _space;
+        public TrainWagon(bool hasCarnivore, int space)
+        {
+            this._hasCarnivore = hasCarnivore;
+            this._space = space;
+        }
+
+        public bool hasCarnivore
+        {
+            get
+            {
+                return _hasCarnivore;
+            }
+            set
+            {
+                _hasCarnivore = value;
+            }
+        }
+        public int space
+        {
+            get
+            {
+                return _space;
+            }
+            set
+            {
+                _space = value;
+            }
+        }
+    }
+}
