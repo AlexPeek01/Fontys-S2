57c76e89de8863300789d8bfec9a7d281d052e6d
Alex Peek <I430284@fhict.nl>
2020-03-17 11:44:47 +0100

    Added unit tests to Algorithmic assignment

--- Algoritmiek/Arithmetic_Casus_CircusAnimals/Arithmetic_Casus_CircusAnimals/MainWindow.xaml.cs
+++ Algoritmiek/Arithmetic_Casus_CircusAnimals/Arithmetic_Casus_CircusAnimals/MainWindow.xaml.cs
@@ -30,12 +30,12 @@ namespace Arithmetic_Casus_CircusAnimals
         {
             Train.CreateTrain(DAL.MySQLManager.GetTrainCount());
             string animalString = "";
-            Animal.PlaceAnimalInWagon();
+            Wagon.PlaceAnimalsInWagon(MainLogic.SortList(Animal.oldAnimalList), DAL.MySQLManager.GetTrainCount());
             foreach (Wagon w in Wagon.wagonList)
             {
-                DisplayBox.Text += "Wagon: " + w.wagonId.ToString() + " Contains:" + '\n';
+                DisplayBox.Text += "Wagon: " + w._wagonId.ToString() + " Contains:" + '\n';
                 for (int i = 0; i < w.animalsInWagon.Count(); i++)
-                    animalString += "   - " +  w.animalsInWagon[i].AnimalName + '\n';
+                    animalString += "   - " +  w.animalsInWagon[i]._animalName + '\n';
                 DisplayBox.Text += animalString + '\n';
                 animalString = "";
             }
@@ -44,37 +44,37 @@ namespace Arithmetic_Casus_CircusAnimals
         }
         private void SmallHerbBtn_Click(object sender, RoutedEventArgs e)
         {
-            Animal.CreateAnimal(false, 1, "Small_Herbivore", false);
+            Animal.CreateAnimal(false, 1, "Small_Herbivore");
             SmallHerbBtn.Content = (Convert.ToInt32(SmallHerbBtn.Content) + 1).ToString();
         }
 
         private void SmallCarnBtn_Click(object sender, RoutedEventArgs e)
         {
-            Animal.CreateAnimal(true, 1, "Small_Carnivore", false);
+            Animal.CreateAnimal(true, 1, "Small_Carnivore");
             SmallCarnBtn.Content = (Convert.ToInt32(SmallCarnBtn.Content) + 1).ToString();
         }
 
         private void MediumHerbBtn_Click(object sender, RoutedEventArgs e)
         {
-            Animal.CreateAnimal(false, 3, "Medium_Herbivore", false);
+            Animal.CreateAnimal(false, 3, "Medium_Herbivore");
             MediumHerbBtn.Content = (Convert.ToInt32(MediumHerbBtn.Content) + 1).ToString();
         }
 
         private void MediumCarnBtn_Click(object sender, RoutedEventArgs e)
         {
-            Animal.CreateAnimal(true, 3, "Medium_Carnivore", false);
+            Animal.CreateAnimal(true, 3, "Medium_Carnivore");
             MediumCarnBtn.Content = (Convert.ToInt32(MediumCarnBtn.Content) + 1).ToString();
         }
 
         private void LargeHerbBtn_Click(object sender, RoutedEventArgs e)
         {
-            Animal.CreateAnimal(false, 5, "Large_Herbivore", false);
+            Animal.CreateAnimal(false, 5, "Large_Herbivore");
             LargeHerbBtn.Content = (Convert.ToInt32(LargeHerbBtn.Content) + 1).ToString();
         }
 
         private void LargeCarnBtn_Click(object sender, RoutedEventArgs e)
         {
-            Animal.CreateAnimal(true, 5, "Large_Carnivore", false);
+            Animal.CreateAnimal(true, 5, "Large_Carnivore");
             LargeCarnBtn.Content = (Convert.ToInt32(LargeCarnBtn.Content) + 1).ToString();
         }
     }
---
+++ Algoritmiek/Arithmetic_Casus_CircusAnimals/Arithmetic_Casus_CircusAnimalsUnitTests/Logic/WagonTests.cs
@@ -0,0 +1,49 @@
+﻿using Arithmetic_Casus_CircusAnimals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_Casus_CircusAnimalsUnitTests.Logic
+{
+    [TestClass()]
+    class WagonTests
+    {
+        List<Animal> animalList;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            animalList = new List<Animal>();
+        }
+
+        [TestMethod()]
+        public void PlaceAnimalsInWagonTest()
+        {
+            //Arange
+            Animal animal1 = new Animal(true, 5, "LargeCarnivore");
+            Animal animal2 = new Animal(true, 3, "MediumCarnivore");
+            Animal animal3 = new Animal(true, 1, "SmallCarnivore");
+            Animal animal4 = new Animal(false, 5, "LargeHerbivore");
+            Animal animal5 = new Animal(false, 1, "SmallHerbivore");
+            animalList.Add(animal1);
+            animalList.Add(animal2);
+            animalList.Add(animal3);
+            animalList.Add(animal4);
+            animalList.Add(animal5);
+
+            //Act
+            Arithmetic_Casus_CircusAnimals.Wagon.PlaceAnimalsInWagon(animalList, 0);
+
+            //Assert
+            Assert.Fail();
+            Assert.IsTrue
+            (Arithmetic_Casus_CircusAnimals.Wagon.wagonList[0].animalsInWagon.Contains(animal1) &&
+            Arithmetic_Casus_CircusAnimals.Wagon.wagonList[1].animalsInWagon.Contains(animal2) &&
+            Arithmetic_Casus_CircusAnimals.Wagon.wagonList[1].animalsInWagon.Contains(animal4) &&
+            Arithmetic_Casus_CircusAnimals.Wagon.wagonList[2].animalsInWagon.Contains(animal3) &&
+            Arithmetic_Casus_CircusAnimals.Wagon.wagonList[3].animalsInWagon.Contains(animal5));
+        }
+    }
+}
