a750ba58a71646e79a2aafe5f71dabff1ede5b06
Alex Peek <I430284@fhict.nl>
2020-03-23 11:28:19 +0100

    Worked on layer structure

---
+++ Algoritmiek/Arithmetic_Casus_CircusAnimals/LogicLayer/Train.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arithmetic_Casus_CircusAnimals.DataAccess;
+
+namespace Arithmetic_Casus_CircusAnimals
+{
+    public class Train
+    {
+        private List<Wagon> wagonsInTrain;
+        private int trainId { get; set; }
+        public Train(int _trainId)
+        {
+            wagonsInTrain = new List<Wagon>();
+            this.trainId = _trainId;
+        }
+        public int _trainId
+        {
+            get { return trainId; }
+            set { trainId = value; }
+        }
+
+        public static void CreateTrain(int _trainId)
+        {
+            Train train = new Train(_trainId);
+        }
+        public static int GetTrainCount()
+        {
+            return MySQLManager.GetTrainCount();
+        }
+    }
+}
---
+++ Projecten/Favours/Favours.Logic/UserLogic.cs
@@ -0,0 +1,18 @@
+﻿using Favours.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Favours.Logic
+{
+    public class UserLogic
+    {
+
+        public string getUserByName(string name)
+        {
+            UserData userData = new UserData();
+            return userData.getUserByName(name);
+        }
+
+    }
+}
