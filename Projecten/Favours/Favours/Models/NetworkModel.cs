321749d3b8f8918db0ed020f7464a4de85700c00
Alex Peek <I430284@fhict.nl>
2020-04-03 15:06:14 +0200

    Iteratie 2 Update

---
+++ Projecten/Favours/Favours/Models/NetworkModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Favours.Models
+{
+    public class NetworkModel
+    {
+        private int networkId { get; set; }
+        private string networkName { get; set; }
+        private List<EventModel> eventList { get; set; }
+        public NetworkModel(int _networkId, string _networkName)
+        {
+            this.networkId = _networkId;
+            this.networkName = _networkName;
+            userList = new List<UserModel>();
+            categoryList = new List<CategorieModel>();
+            eventList = new List<EventModel>();
+        }
+    }
+}
