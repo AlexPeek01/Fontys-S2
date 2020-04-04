df7a14d792986c6c49453d29b3c9b8a500bade64
Alex Peek <I430284@fhict.nl>
2020-04-04 22:48:36 +0200

    Started attempt 4 at containership

---
+++ Algoritmiek/ContainerShip/Logic/Container.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public class Container
+    {
+        private int weight;
+        private bool cooled;
+        private bool valuable;
+        private bool placed;
+        
+        public Container(int _weight, bool _cooled, bool _valuable, bool _placed)
+        {
+            this.weight = _weight;
+            this.cooled = _cooled;
+            this.valuable = _valuable;
+            this.placed = _placed;
+        }
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public bool Cooled
+        {
+            get { return cooled; }
+            set { cooled = value; }
+        }
+        public bool Valuable
+        {
+            get { return valuable; }
+            set { valuable = value; }
+        }
+        public bool Placed
+        {
+            get { return placed; }
+            set { placed = value; }
+        }
+    }
+}
