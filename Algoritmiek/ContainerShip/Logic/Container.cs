9a8e0299d5af3deb7319580583dc07a8497cbd7f
Alex Peek <I430284@fhict.nl>
2020-04-11 16:15:12 +0200

    Analyse document verbeterd

--- Algoritmiek/ContainerShip/Logic/Container.cs
+++ Algoritmiek/ContainerShip/Logic/Container.cs
@@ -6,37 +6,19 @@ namespace Logic
 {
     public class Container
     {
-        private int weight;
-        private bool cooled;
-        private bool valuable;
-        private bool placed;
+        public int weight { get; set; }
+        public bool cooled { get; set; }
+        public bool valuable { get; set; }
+        public bool placed { get; set; }
+        public bool valuableBlocked { get; set; }
         
-        public Container(int _weight, bool _cooled, bool _valuable, bool _placed)
+        public Container(int _weight, bool _cooled, bool _valuable, bool _placed, bool _valuableBlocked)
         {
             this.weight = _weight;
             this.cooled = _cooled;
             this.valuable = _valuable;
             this.placed = _placed;
-        }
-        public int Weight
-        {
-            get { return weight; }
-            set { weight = value; }
-        }
-        public bool Cooled
-        {
-            get { return cooled; }
-            set { cooled = value; }
-        }
-        public bool Valuable
-        {
-            get { return valuable; }
-            set { valuable = value; }
-        }
-        public bool Placed
-        {
-            get { return placed; }
-            set { placed = value; }
+            this.valuableBlocked = _valuableBlocked;
         }
     }
 }
