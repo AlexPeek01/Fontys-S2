758412008b9f3a6ac0b8e115d38cb9a578feb413
Alex Peek <I430284@fhict.nl>
2020-04-23 22:31:41 +0200

    Updated all files in Ontwerp document and added comments

--- Projecten/Favours/Favours.Logic/Network.cs
+++ Projecten/Favours/Favours.Logic/Network.cs
@@ -7,25 +7,20 @@ namespace Favours.Logic
 {
     public class Network
     {
-        private int netwerkId { get; set; }
-        private string netwerkNaam { get; set; }
-        private string wachtwoord { get; set; }
-        private List<Event> eventList { get; set; }
-        private List<Category> categoryList { get; set; }
-        private List<User> userList { get; set; }
-        public Network(string _netwerkNaam)
+        private int networkId;
+        private string networkName;
+        private string password;
+        private int memberLimit;
+        private bool visibility;
+        private List<Service> eventList;
+        private List<Category> categoryList;
+        private List<User> userList;
+        public Network(string _networkName)
         {
-            this.netwerkNaam = _netwerkNaam;
+            this.networkName = _networkName;
             userList = new List<User>();
             categoryList = new List<Category>();
-            eventList = new List<Event>();
-        }
-        public string NetwerkNaam
-        {
-            get
-            {
-                return netwerkNaam;
-            }
+            eventList = new List<Service>();
         }
     }
 }
---
+++ Projecten/Favours/Favours.Logic/Service.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Favours.Logic
+{
+    public class Service
+    {
+        private int serviceId;
+        private int postersId;
+        private string title;
+        private string description;
+        private string[] images;
+        private DateTime date;
+        private bool visibility;
+
+        public Service(int _postersId, string _title, string _description)
+        {
+            images = new string[9];
+            this.postersId = _postersId;
+            this.title = _title;
+            this.description = _description;
+        }
+    }
+}
