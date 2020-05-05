31f71185b25316768a262adda02191be9d057f3f
Alex Peek <I430284@fhict.nl>
2020-05-05 17:32:47 +0200

    Improved responsiveness and turned app into pwa

---
+++ Projecten/FavoursApp/FavoursApp/Models/NetworkModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FavoursApp.Models
+{
+    public class NetworkModel
+    {
+        public string networkName;
+        public NetworkModel()
+        {
+
+        }
+    }
+}
