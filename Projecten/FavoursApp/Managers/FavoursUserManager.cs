935bfd15748f0eb45e77ec987355e01c24d7d32b
Alex Peek <I430284@fhict.nl>
2020-05-26 15:46:31 +0200

    Made some algorithm assignments

--- Projecten/FavoursApp/Managers/FavoursUserManager.cs
+++ Projecten/FavoursApp/Managers/FavoursUserManager.cs
@@ -1,4 +1,5 @@
 ﻿using DAL;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +8,14 @@ namespace Managers
 {
     public class FavoursUserManager
     {
+        UserDB userDB = new UserDB();
         public void InsertNewProfileData(string id)
         {
             UserDB.InsertNewProfileData(id);
+        }
+        public User GetUserDataByUserID(string userId)
+        {
+            return userDB.GetUserDataByUserID(userId);
         }
     }
 }
--- Projecten/FavoursApp/Models/User.cs
+++ Projecten/FavoursApp/Models/User.cs
@@ -6,14 +6,12 @@ namespace Models
 {
     public class User
     {
-        public int UserId { get; private set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string UserId { get; private set; }
         public string FirstName { get; set; }
         public string Insertion { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
-        public User()
+        public User(string UserId)
         {
             
         }
