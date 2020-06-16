373e62fea3874db7da0b65c1099ab9b428ca6c18
Alex Peek <I430284@fhict.nl>
2020-06-16 16:37:57 +0200

    Added email functionality

--- Projecten/FavoursApp/FavoursApp/Controllers/ServiceController.cs
+++ Projecten/FavoursApp/FavoursApp/Controllers/ServiceController.cs
@@ -18,14 +18,20 @@ namespace FavoursApp.Controllers
     {
         private readonly IServiceManager servicemanager;
         private readonly IUserManager usermanager;
+        private readonly CommunicationManager communicationmanager;
         public ServiceController(IConfiguration config)
         {
             Factory factory = new Factory();
             servicemanager = factory.GetServiceManager(config["HandlerType"]);
             usermanager = factory.GetUserManager(config["HandlerType"]);
+            communicationmanager = new CommunicationManager();
         }
         public IActionResult Service(string serviceID)
         {
+            // Input checks
+            if (string.IsNullOrEmpty(serviceID)) return RedirectToAction("Index", "Network");
+
+            // Assembling ServiceWithUserModel
             Service service = servicemanager.GetServiceDataById(serviceID);
             User user = usermanager.GetUserData(service.PostersID);
             return View(new ServiceWithUser(user, service));
@@ -33,14 +39,18 @@ namespace FavoursApp.Controllers
         [HttpGet]
         public IActionResult Message(string posterid, string serviceid)
         {
+            // Input checks
             if (string.IsNullOrEmpty(posterid) || string.IsNullOrEmpty(serviceid)) return RedirectToAction("Index", "Network");
+
+            // Assembling ServiceWithUserModel
             Service service = servicemanager.GetServiceDataById(serviceid);
             User user = usermanager.GetUserData(posterid);
             return View(new ServiceWithUser(user, service));
         }
         [HttpPost]
-        public IActionResult Message(string email, string message, string overloading)
+        public IActionResult Message(string email, string message, string name)
         {
+            communicationmanager.SendMail(email, name, message);
             return RedirectToAction("Index", "Network");
         }
     }
---
+++ Projecten/FavoursApp/Managers/CommunicationManager.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Managers
+{
+    public class CommunicationManager
+    {
+        public void SendMail(string toEmail, string toName, string message)
+        {
+            var client = new RestClient("https://api.sendgrid.com/v3/mail/send");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("authorization", "Bearer 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");
+            request.AddParameter("application/json", "{\"personalizations\":[{\"to\":[{\"email\":\"" + toEmail + "\",\"name\":\"" + toName + "\"}],\"dynamic_template_data\":{\"message\":\"" + message + "\"},\"subject\":\"Wachtwoord reset\"}],\"from\":{\"email\":\"noreply@favours.com\",\"name\":\"Favours\"},\"reply_to\":{\"email\":\"noreply@favours.com\",\"name\":\"Favours\"},\"template_id\":\"d-19361b2c4c9643e3a98e1cc9b5496131\"}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+        }
+    }
+}
