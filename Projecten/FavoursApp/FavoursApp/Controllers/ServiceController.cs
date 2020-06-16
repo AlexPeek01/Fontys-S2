1d55340ecaaf2313278449969b6a5e5ea6b651da
Alex Peek <I430284@fhict.nl>
2020-06-16 15:52:39 +0200

    Added features / updated design documents

--- Projecten/FavoursApp/FavoursApp/Controllers/ServiceController.cs
+++ Projecten/FavoursApp/FavoursApp/Controllers/ServiceController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using AdditionalFiles.Interfaces.IManagers;
 using AdditionalFiles.Interfaces.IRepos;
+using FavoursApp.Models;
 using Managers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Models;
@@ -15,14 +17,31 @@ namespace FavoursApp.Controllers
     public class ServiceController : Controller
     {
         private readonly IServiceManager servicemanager;
+        private readonly IUserManager usermanager;
         public ServiceController(IConfiguration config)
         {
             Factory factory = new Factory();
             servicemanager = factory.GetServiceManager(config["HandlerType"]);
+            usermanager = factory.GetUserManager(config["HandlerType"]);
         }
         public IActionResult Service(string serviceID)
         {
-            return View(servicemanager.GetServiceDataById(serviceID));
+            Service service = servicemanager.GetServiceDataById(serviceID);
+            User user = usermanager.GetUserData(service.PostersID);
+            return View(new ServiceWithUser(user, service));
+        }
+        [HttpGet]
+        public IActionResult Message(string posterid, string serviceid)
+        {
+            if (string.IsNullOrEmpty(posterid) || string.IsNullOrEmpty(serviceid)) return RedirectToAction("Index", "Network");
+            Service service = servicemanager.GetServiceDataById(serviceid);
+            User user = usermanager.GetUserData(posterid);
+            return View(new ServiceWithUser(user, service));
+        }
+        [HttpPost]
+        public IActionResult Message(string email, string message, string overloading)
+        {
+            return RedirectToAction("Index", "Network");
         }
     }
 }
---
+++ Projecten/FavoursApp/FavoursApp/Models/ServiceWithUser.cs
@@ -0,0 +1,20 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FavoursApp.Models
+{
+    public class ServiceWithUser
+    {
+        public User user { get; private set; }
+        public Service service { get; private set; }
+        public ServiceWithUser(User _user, Service _service)
+        {
+            user = _user;
+            service = _service;
+        }
+        public ServiceWithUser(){}
+    }
+}
